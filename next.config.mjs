ec81cf56566ced0c401b073e082ce106ddfba75b
Monika Rajasekaran <126960056+MonikaRajasekaran@users.noreply.github.com>
2025-11-05 17:30:54 +0530

    Add files via upload
    First Commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['miniture.b-cdn.net','cdn.pixabay.com','a0.muscache.com','rukminim1.flixcart.com'],
+  },
+};
+
+export default nextConfig;
